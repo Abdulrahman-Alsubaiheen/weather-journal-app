225a2a9274691758b6544fb5e8426e6bd0cd66d3
aboXmsa3d <aboXmsa3d@outlook.com>
2021-01-07 16:58:56 +0300

    format the code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ projectData = {};
 
 // Start up an instance of app
 
+/* Dependencies */
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,3 +18,12 @@ app.use(express.static('website'));
 
 
 // Setup Server
+
+// Callback to debug
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
+  
