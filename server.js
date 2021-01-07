2ef0a88cccbc166cb0e2bf192f3d18c03edda61c
aboXmsa3d <aboXmsa3d@outlook.com>
2021-01-07 17:04:21 +0300

    Setup the Server

--- server.js
+++ server.js
@@ -2,28 +2,38 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Dependencies */
-/* Middleware*/
+/* Dependencies */ /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
 
-// Callback to debug
+const server = app.listen(port, listening);
 
-// Initialize all route with a callback function
+function listening(){
+console.log(`running on localhost: ${port}`);
+};
 
-// Callback function to complete GET '/all'
+// ~~~~~~ Routes ~~~~~~
 
-// Post Route
-  
+// ( GET )
+
+
+
+// ( Post )
