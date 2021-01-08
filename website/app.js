256c479ccae1e1b8b8f7806222227a7fde1e5b0e
aboXmsa3d <aboXmsa3d@outlook.com>
2021-01-08 14:01:02 +0300

    Done

--- website/app.js
+++ website/app.js
@@ -1,23 +1,71 @@
 /* Global Variables */
-
-// Personal API Key for OpenWeatherMap API
-
-
-// Event listener to add function to existing HTML DOM element
-
-
-/* Function called by event listener */
-
-
-/* Function to GET Web API Data*/
-
-
-/* Function to POST data */
-
-
-/* Function to GET Project Data */
-
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const API_Key = '6747d09c54361a4d24e74617a2659285';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// ~~~~~~ Event listener ~~~~~~
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+    const zip =  document.getElementById('zip').value;
+    const feelings =  document.getElementById('feelings').value;
+  
+    getData(baseUrl , zip , API_Key)
+    .then(function(data){
+      postData('/data', {date: newDate, feelings: feelings , temp: data.main.temp} );
+    })
+    .then(
+      updateUI()
+    )
+  }
+
+// ~~~~~~ ( GET ) Web API Data ~~~~~~
+const getData = async (url, zip, API_Key) => {
+    const response = await fetch(`${url}${zip}&appid=${API_Key}`)
+    try {
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log('error', err);
+    }
+  }
+
+
+// ~~~~~~ ( POST ) data ~~~~~~
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+  };
+
+// ~~~~~~ ( update UI ) ~~~~~~
+
+const updateUI = async () => {
+    const request = await fetch('/data');
+    try{
+      const Data = await request.json();
+      document.getElementById('date').innerHTML = Data.date;
+      document.getElementById('temp').innerHTML = Data.temp;
+      document.getElementById('content').innerHTML = Data.feelings;
+  
+    }catch(error){
+      console.log("error", error);
+    }
+  }
